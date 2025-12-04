7235f209ad41b776e7c608c2a46d3a5ca25df77a
dylantoplas <toplas.d@northeastern.edu>
2025-12-04 17:15:26 -0500

    final methods

--- r_method/bayesian.R
+++ r_method/bayesian.R
@@ -1,232 +1,179 @@
-
-data <- read.csv('../bonds_dashboards/fred_md.csv')
-epu <- read.csv('../bonds_dashboards/epu_index.csv')
-head(epu)
-data$sasdate <- as.Date(data$sasdate, format = "%m/%d/%Y")
-epu$DATE <- as.Date(epu$DATE)
-epu$DATE <- epu$DATE + 1
-data <- merge(data, epu, 
-                       by.x = "sasdate", 
-                       by.y = "DATE",
-                       all = FALSE)# all=FALSE means inner join
-auction <- read.csv('/Users/dylantoplas/Documents/ds4420/DS4420Project/bonds_dashboards/monthly_auction.csv')
-head(auction)
-auction$record_date <- as.Date(paste0(auction$record_date, "-01"), format = "%Y-%m-%d")
-data <- merge(data, auction, 
-              by.x = "sasdate", 
-              by.y = "record_date",
-              all = FALSE)# all=FALSE means inner join
-
-nrow(data)
-# ==============================================================================================================
 library(brms)
-
-# Fitting a normal model
-# with some bells and whistles (lots of different options, see: https://cran.r-project.org/web/packages/brms/brms.pdf)
-# For convenience, we'll use the default priors (you can check the docs for adjusting these as necessary)
-
-features <- c('UNRATE', 'CPIAUCSL', 'INDPRO', 'PAYEMS', 'HOUST', 
-              'USEPUINDXD', 'bid_to_cover_ratio_long', 'FEDFUNDS')
-
-# Scale data
-data_scaled <- data
-data_scaled$GS10_lag <- c(0, head(data$GS10, -1))
-data_scaled[, features] <- scale(data_scaled5[, features])  
-
-# Build formula dynamically
-formula_str <- paste("GS10 ~", paste(c(features, "GS10_lag"), collapse = " + "))
-formula_obj <- as.formula(formula_str)
-
-# Use the formula
-prior <- default_prior(formula_obj, data = data_scaled, family = gaussian())
-
-gs10_brm <- brm(formula_obj,
-                family = gaussian(),
-                data = data_scaled,
-                chains = 4,
-                cores = getOption("mc.cores", 1),
-                iter = 4000,
-                warmup = 720,
-                thin = 2,
-                prior = prior)
-
-# Summary and diagnostics
-# summary(gs10_brm)
-# plot(gs10_brm)
-bayes_R2(gs10_brm)
-
-# Posterior predictions
-post_preds <- posterior_predict(gs10_brm)
-rate_post_preds <- post_preds[, nrow(data_scaled)]  
-# hist(rate_post_preds)
-# mean(rate_post_preds)
-
-# Predict next observation - dynamically build new_data
-last_obs <- tail(data_scaled, 1)
-new_data <- as.data.frame(last_obs[, features])
-new_data$GS10_lag <- last_obs$GS10
-
-# Predict
-future_pred <- posterior_predict(gs10_brm, newdata = new_data)
-
-# GS1
-data_scaled1 <- data
-data_scaled1$GS1_lag <- c(0, head(data$GS1, -1))
-data_scaled1[, features] <- scale(data_scaled1[, features]) 
-
-# Build formula dynamically
-formula_str <- paste("GS1 ~", paste(c(features, "GS1_lag"), collapse = " + "))
-formula_obj <- as.formula(formula_str)
-
-# Use the formula
-prior1 <- default_prior(formula_obj, data = data_scaled1, family = gaussian())
-
-gs1_brm <- brm(formula_obj,
-                family = gaussian(),
-                data = data_scaled51,
-                chains = 4,
-                cores = getOption("mc.cores", 1),
-                iter = 4000,
-                warmup = 720,
-                thin = 2,
-                prior = prior1)
-
-# Summary and diagnostics
-# summary(gs1_brm)
-# plot(gs1_brm)
-bayes_R2(gs1_brm)
-
-# Posterior predictions
-post_preds1 <- posterior_predict(gs1_brm)
-rate_post_preds1 <- post_preds1[, nrow(data_scaled1)]  
-# hist(rate_post_preds1)
-# mean(rate_post_preds1)
-
-# Predict next observation - dynamically build new_data
-last_obs <- tail(data_scaled1, 1)
-new_data <- as.data.frame(last_obs[, features])
-new_data$GS1_lag <- last_obs$GS1
-
-# Predict
-future_pred1 <- posterior_predict(gs1_brm, newdata = new_data)
-# hist(future_pred1, main = "Prediction for Next Month")
-mean(future_pred1)  # Should be close to current rate (4.26%)
-quantile(future_pred1, c(0.022, 0.972))  # 92% prediction interval
-
-
-
-# GS5
-data_scaled5 <- data
-data_scaled5$GS5_lag <- c(0, head(data$GS5, -1))
-data_scaled5[, features] <- scale(data_scaled5[, features]) 
-
-# Build formula dynamically
-formula_str <- paste("GS5 ~", paste(c(features, "GS5_lag"), collapse = " + "))
-formula_obj <- as.formula(formula_str)
-
-# Use the formula
-prior <- default_prior(formula_obj, data = data_scaled5, family = gaussian())
-
-gs5_brm <- brm(formula_obj,
-                family = gaussian(),
-                data = data_scaled5,
-                chains = 4,
-                cores = getOption("mc.cores", 1),
-                iter = 4000,
-                warmup = 720,
-                thin = 2,
-                prior = prior)
-
-# Summary and diagnostics
-# summary(gs5_brm)
-# plot(gs5_brm)
-bayes_R2(gs5_brm)
-
-# Posterior predictions
-post_preds5 <- posterior_predict(gs5_brm)
-rate_post_preds5 <- post_preds5[, nrow(data_scaled5)]  
-# hist(rate_post_preds5)
-# mean(rate_post_preds5)
-
-# Predict next observation - dynamically build new_data
-last_obs5 <- tail(data_scaled5, 1)
-new_data5 <- as.data.frame(last_obs5[, features])
-new_data5$GS5_lag <- last_obs5$GS5
-
-# Predict
-future_pred5 <- posterior_predict(gs5_brm, newdata = new_data5)
-# hist(future_pred5, main = "Prediction for Next Month")
-mean(future_pred5)  # Should be close to current rate (4.56%)
-quantile(future_pred5, c(0.055, 0.975))  # 95% prediction interval
-
-
-# GS2 
+# ======================================================
+# Getting data
+data <- read.csv('/Users/dylantoplas/Documents/ds4420/DS4420Project/bonds_dashboards/fred_md.csv')
+epu <- read.csv('/Users/dylantoplas/Documents/ds4420/DS4420Project/bonds_dashboards/epu_index.csv')
+auction <- read.csv('/Users/dylantoplas/Documents/ds4420/DS4420Project/bonds_dashboards/monthly_auction.csv')
 g2 <- read.csv('/Users/dylantoplas/Documents/ds4420/DS4420Project/bonds_dashboards/GS2.csv')
 
-head(g2)
-names(g2)
-# data <- read.csv('/Users/dylantoplas/Documents/ds4420/DS4420Project/bonds_dashboards/fred_md.csv')
-# data <- data[-1, ]
-
-# Check date formats
-head(data$sasdate)
-head(g2$observation_date)
-
-# Convert dates to same format if needed
+data$sasdate <- as.Date(data$sasdate, format = "%m/%d/%Y")
+epu$DATE <- as.Date(epu$DATE) + 1
+auction$record_date <- as.Date(paste0(auction$record_date, "-01"), format = "%Y-%m-%d")
 g2$observation_date <- as.Date(g2$observation_date)
 
-# Inner join - keeps only matching dates
-data_with_gs2 <- merge(data, g2, 
-                       by.x = "sasdate", 
-                       by.y = "observation_date",
-                       all = FALSE)# all=FALSE means inner join
+data <- merge(data, epu, by.x = "sasdate", by.y = "DATE", all = FALSE)
+data <- merge(data, auction, by.x = "sasdate", by.y = "record_date", all = FALSE)
+data_with_gs2 <- merge(data, g2, by.x = "sasdate", by.y = "observation_date", all = FALSE)
 
+# ======================================================
 
-
-# Now scale and add lags
-data_scaled2 <- data_with_gs2
-data_scaled2$GS2_lag <- c(0, head(data_with_gs2$GS2, -1))
-data_scaled2[, features] <- scale(data_scaled2[, features]) 
-
-# Build formula dynamically
-formula_str <- paste("GS2 ~", paste(c(features, "GS2_lag"), collapse = " + "))
-formula_obj <- as.formula(formula_str)
-
-# Use the formula
-prior2 <- default_prior(formula_obj, data = data_scaled2, family = gaussian())
-
-gs2_brm <- brm(formula_obj,
+fit_bayesian_model <- function(data, target_var, features, 
+                               chains = 4, iter = 1000*4, warmup = 180*4, thin = 2,
+                               cores = 4, verbose = FALSE) {
+  
+  lag_var <- paste0(target_var, "_lag")
+  data[[lag_var]] <- c(0, head(data[[target_var]], -1))
+  
+  data[, features] <- scale(data[, features])
+  
+  formula_str <- paste(target_var, "~", paste(c(features, lag_var), collapse = " + "))
+  formula_obj <- as.formula(formula_str)
+  
+  prior <- default_prior(formula_obj, data = data, family = gaussian())
+  
+  model <- brm(formula_obj,
                family = gaussian(),
-               data = data_scaled2,
-               chains = 4,
-               cores = getOption("mc.cores", 1),
-               iter = 4000,
-               warmup = 720,
-               thin = 2,
-               prior = prior2)
+               data = data,
+               chains = chains,
+               cores = cores,
+               iter = iter,
+               warmup = warmup,
+               thin = thin,
+               prior = prior,
+               )
+  
+  return(list(model = model, data_scaled = data, features = features, target_var = target_var))
+}
 
-# Summary and diagnostics
-# summary(gs2_brm)
-# plot(gs2_brm)
-bayes_R2(gs2_brm)
+# ====================================================
 
-# Posterior predictions
-post_preds2 <- posterior_predict(gs2_brm)
-rate_post_preds2 <- post_preds2[, nrow(data_scaled2)]  
-# hist(rate_post_preds2)
-# mean(rate_post_preds2)
+test_bayesian_model <- function(result, test_size = 12) {
+  model <- result$model
+  data_scaled <- result$data_scaled
+  target_var <- result$target_var
+  features <- result$features
+  
+  # Split data
+  train_data <- head(data_scaled, nrow(data_scaled) - test_size)
+  test_data <- tail(data_scaled, test_size)
+  
+  # Predict on test set
+  test_predictions <- predict(model, newdata = test_data, probs = c(0.025, 0.975))
+  
+  # Calculate metrics
+  actual <- test_data[[target_var]]
+  predicted <- test_predictions[, "Estimate"]
+  lower_95 <- test_predictions[, "Q2.5"]
+  upper_95 <- test_predictions[, "Q97.5"]
+  
+  rmse <- sqrt(mean((actual - predicted)^2))
+  mae <- mean(abs(actual - predicted))
+  
+  cat("Testing for", target_var, "\n")
+  cat("RMSE:                 ", round(rmse, 4), "\n")
+  cat("MAE:                  ", round(mae, 4), "\n")
 
-# Predict next observation - dynamically build new_data
-last_obs2 <- tail(data_scaled2, 1)
-new_data2 <- as.data.frame(last_obs2[, features])
-new_data2$GS2_lag <- last_obs2$GS2
+    return(list(actual = actual,
+                predicted = predicted,
+                lower_95 = lower_95,
+                upper_95 = upper_95,
+                rmse = rmse,
+                mae = mae,
+                target_var = target_var))
+  
+}
+  
+plot_test_results <- function(test_result) {
+  test_size <- length(test_result$actual)
+  
+  plot(1:test_size, test_result$actual, type = "b", pch = 19, 
+       ylim = range(c(test_result$actual, test_result$lower_95, test_result$upper_95)),
+       xlab = "Month", ylab = test_result$target_var,
+       main = paste(test_result$target_var, "- Out-of-Sample Test"))
+  lines(1:test_size, test_result$predicted, col = "red", type = "b", pch = 17)
+  
+  polygon(c(1:test_size, rev(1:test_size)), 
+          c(test_result$lower_95, rev(test_result$upper_95)),
+          col = rgb(1, 0, 0, 0.2), border = NA)
+  
+  legend("bottomleft", c("Actual", "Predicted"), 
+         col = c("black", "red", rgb(1, 0, 0)), 
+         lty = c(1, 1, 1), pch = c(19, 17),
+         cex = 0.5)
+}
 
-# Predict
-future_pred2 <- posterior_predict(gs2_brm, newdata = new_data2)
-# hist(future_pred2, main = "Prediction for Next Month")
-mean(future_pred2)  # Should be close to current rate (4.26%)
-quantile(future_pred2, c(0.022, 0.972))  # 92% prediction interval
+macro_features <- c('UNRATE', 'CPIAUCSL', 'INDPRO', 'PAYEMS', 'HOUST', 'USEPUINDXD')
 
-yield_curve <- c(mean(rate_post_preds1),mean(rate_post_preds2),mean(rate_post_preds5),mean(rate_post_preds))
+auction_features <- c('bid_to_cover_ratio_', 'direct_bidder_tendered_', 
+                      'indirect_bidder_tendered_', 'primary_dealer_tendered_',
+                      'comp_tendered_', 'direct_bidder_accepted_', 
+                      'indirect_bidder_accepted_', 'primary_dealer_accepted_', 
+                      'comp_accepted_')
+
+auction_features_long <- paste0(auction_features, "long")
+auction_features_bills <- paste0(auction_features, "bills")
+auction_features_medium <- paste0(auction_features, "medium")
+auction_features_short <- paste0(auction_features, "short")
+
+features_gs10 <- c(macro_features, 
+                   auction_features_long, 'FEDFUNDS')
+
+features_gs1 <- c(macro_features, 
+                  auction_features_bills, 'FEDFUNDS')
+
+features_gs5 <- c(macro_features, 
+                  auction_features_medium, 'FEDFUNDS')
+
+features_gs2 <- c(macro_features, 
+                  auction_features_short, 'FEDFUNDS')
+
+result_gs10 <- fit_bayesian_model(data, "GS10", features_gs10)
+result_gs1 <- fit_bayesian_model(data, "GS1", features_gs1)
+result_gs5 <- fit_bayesian_model(data, "GS5", features_gs5)
+result_gs2 <- fit_bayesian_model(data_with_gs2, "GS2", features_gs2)
+
+test_gs10 <- test_bayesian_model(result_gs10)
+test_gs1 <- test_bayesian_model(result_gs1)
+test_gs5 <- test_bayesian_model(result_gs5)
+test_gs2 <- test_bayesian_model(result_gs2)
+
+# For poster
+par(mfrow = c(2, 2))
+plot_test_results(test_gs10)
+plot_test_results(test_gs1)
+plot_test_results(test_gs5)
+plot_test_results(test_gs2)
+
+
+comparison <- data.frame(
+  Model = c("GS1", "GS2", "GS5", "GS10"),
+  RMSE = c(test_gs1$rmse, test_gs2$rmse, test_gs5$rmse, test_gs10$rmse),
+  MAE = c(test_gs1$mae, test_gs2$mae, test_gs5$mae, test_gs10$mae),
+)
+
+print(comparison)
+
+best_model_rmse <- comparison$Model[which.min(comparison$RMSE)]
+cat("\nBest model by RMSE:", best_model_rmse, "\n")
+
+best_model_mae <- comparison$Model[which.min(comparison$MAE)]
+cat("\nBest model by MAE:", best_model_mae, "\n")
+
+
+predict_next <- function(result) {
+  last_obs <- tail(result$data_scaled, 1)
+  lag_var <- paste0(result$target_var, "_lag")
+  new_data <- last_obs[, c(result$features, lag_var)]
+  pred <- posterior_predict(result$model, newdata = new_data)
+  return(mean(pred))
+}
+
+future_gs1 <- predict_next(result_gs1)
+future_gs2 <- predict_next(result_gs2)
+future_gs5 <- predict_next(result_gs5)
+future_gs10 <- predict_next(result_gs10)
+
+yield_curve <- c(future_gs1, future_gs2, future_gs5, future_gs10)
 yield_curve
-# 4.045306 3.735496 3.735496 3.897070
+# 4.089233 3.766546 3.407099 3.824731
+
