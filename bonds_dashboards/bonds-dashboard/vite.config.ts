94a1ed49eae6dc4e22da86c69625a7737c361c71
dmljb <123711711+dmljb@users.noreply.github.com>
2026-01-14 23:49:45 -0500

    Change base path in Vite configuration

--- bonds_dashboards/bonds-dashboard/vite.config.ts
+++ bonds_dashboards/bonds-dashboard/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/DS4420Project/",
+  base: "/YieldPrediction/",
   plugins: [react()],
 })
